1e12d15d128b3ec4064e4b5d7f9d1c593ccb6469
Nicholas Barrera <nickbarerra722@gmail.com>
2022-10-24 14:08:31 -0500

    update

---
+++ src/main/java/Student.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name){
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    // returns the student's id
+    public long getId(){
+        return id;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return name;
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+        return grades;
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+        this.grades.add(grade);
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        double average = 0;
+        for (int grade: this.grades) {
+            average += grade;
+        }
+        return average / this.grades.size();
+    }
+    }
+
+
+
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+    Student nick;
+
+    @Before
+    public void setup(){
+        nick = new Student(1, "nick");
+    }
+
+
+
+
+
+
+
+
+}
